e9c78f078c8db054035555ae0e1ae343f5cd3600
Christian Baduria <christian.baduria@gmail.com>
2020-01-14 13:21:46 -0500

    Began Classwork 7 (Chapter 13) problems

---
+++ src/classwork/cw7/Sorting.java
@@ -0,0 +1,85 @@
+package classwork.cw7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sorting {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.print("Dictionary Text File: ");
+        List<String> dict = readTextFile(promptFile());
+        /*System.out.println(dict);*/
+        System.out.println("Enter 2 Words from the Dictionary");
+        System.out.print("\tWord 1: ");
+        String a = promptWordInDict(dict);
+        System.out.print("\tWord 2: ");
+        String b = promptWordInDict(dict);
+        int wordDist = getWordDistance(dict, a, b);
+        System.out.printf("The words \"%s\" and \"%s\" are %d words apart in the dictionary.", a, b, wordDist);
+    }
+
+    public static File promptFile() {
+        Scanner s = new Scanner(System.in);
+        File file = new File("src\\text\\" + s.nextLine());
+
+        while (!file.exists()) {
+            System.out.print("File does not exist. Try again: ");
+            file = new File("src\\text\\" + s.nextLine());
+        }
+
+        return file;
+    }
+
+    public static String promptWordInDict(List<String> dict) {
+        Scanner s = new Scanner(System.in);
+        String word = s.next().toLowerCase();
+
+        while (!dict.contains(word)) {
+            System.out.print(word + " not found in dictionary. Enter another word: ");
+            word = s.next();
+        }
+
+        return word;
+    }
+
+    public static List<String> readTextFile(File file) throws FileNotFoundException {
+        Scanner s = new Scanner(file);
+        List<String> text = new ArrayList<String>();
+
+        while (s.hasNextLine())
+            text.add(s.nextLine());
+
+        return text;
+    }
+
+    public static int getWordDistance(List<String> dict, String a, String b) {
+        return Math.abs(dict.indexOf(a) - dict.indexOf(b));
+    }
+
+    public static List<Comparable> bogoSort(List<Comparable> a) {
+        int bigO = 0;
+
+        while (!isSorted(a)) {
+            Collections.shuffle(a);
+            bigO++;
+        }
+
+        System.out.println("Bogo Sort: "+ bigO + " iterations.");
+
+        return a;
+    }
+
+    public static boolean isSorted(List<Comparable> a) {
+        for (int i = 0; i < a.size() - 1; i++) {
+            if (a.get(i).compareTo(a.get(i + 1)) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- src/randomwriter/RandomWriter.java
+++ src/randomwriter/RandomWriter.java
@@ -2,42 +2,69 @@ package randomwriter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class RandomWriter {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // Prompt user for input file, output file, analysis level, and length of output
+    public static void main(String[] args) throws IOException {
+        // Prompt for input file
         Scanner s = new Scanner(System.in);
         System.out.print("Input Source File: ");
-        File inputFile = new File("src\\randomwriter\\" + s.nextLine());
+        File inputFile = promptFile();
+        String input = readTextFile(inputFile);
+        // Prompt for output file
         System.out.print("Output File: ");
-        File outputFile = new File("src\\randomwriter\\" + s.nextLine());
+        File outputFile = promptFile();
+        FileWriter outputFW = new FileWriter(outputFile);
+        // Prompt for analysis level
         System.out.print("Analysis Level: ");
         int k = s.nextInt();
+        while (k < 0 || k > input.length()) {
+            System.out.print("Invalid input. Try again: ");
+            k = s.nextInt();
+        }
+        // Prompt for # of characters to write
         System.out.print("# Characters to Write: ");
         int length = s.nextInt();
+        while (length < 0) {
+            System.out.print("Invalid input. Try again: ");
+            length = s.nextInt();
+        }
         s.close();
 
-        // Initialize input, output, and seed
-        String input = readTextFile(inputFile);
-        StringBuilder output = new StringBuilder(input.substring(0, k));
+        // Initialize output and seed
+        StringBuilder output = new StringBuilder();
         StringBuilder seed = new StringBuilder(input.substring(0, k));
 
         // Loop to determine and write the next character seed (previous k characters)
         for (int i = k; i < length; i++) {
             List<Character> chars = analyzeText(input, seed, k, length);
-            System.out.println(seed + ": " + chars);
             if (chars.size() != 0)
-                output.append(chooseCharacter(chars));
+                outputFW.write(chooseCharacter(chars));
             seed.deleteCharAt(0);
             seed.append(input.charAt(i));
         }
 
-        // Print the output
-        System.out.println(output);
+        outputFW.close();
+
+        // Finished message
+        System.out.println("\n**** Successfully Executed! See output at " + outputFile.getName() + ". ****");
+    }
+
+    public static File promptFile() {
+        Scanner s = new Scanner(System.in);
+        File file = new File("src\\randomwriter\\" + s.nextLine());
+
+        while (!file.exists()) {
+            System.out.print("File does not exist. Try again: ");
+            file = new File("src\\randomwriter\\" + s.nextLine());
+        }
+
+        return file;
     }
 
     // Returns the text, expressed as a string, from the .txt file
@@ -54,7 +81,7 @@ public class RandomWriter {
     // Returns a list of possible characters to append based on a seed
     public static List<Character> analyzeText(String text, StringBuilder seed, int k, int length) {
         List<Character> chars = new ArrayList<Character>();
-        for (int i = 0; i < length - k; i++) {
+        for (int i = 0; i < length; i++) {
             if (text.substring(i, i + k).equals(seed.toString()))
                 chars.add(text.charAt(i + seed.length()));
         }
