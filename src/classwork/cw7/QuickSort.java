013fa0821ece52b0392ea73f1b0cfc47f4b351fc
Christian Baduria <christian.baduria@gmail.com>
2020-01-29 13:16:52 -0500

    Improved efficiency of algorithm

--- src/classwork/cw7/QuickSort.java
+++ src/classwork/cw7/QuickSort.java
@@ -1,14 +1,14 @@
 package classwork.cw7;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class QuickSort {
 
     public static void main(String[] args) {
-        List<Comparable> n = new LinkedList<>(Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8));
+        List<Comparable> n = new ArrayList<>(Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8));
 
         System.out.println("Unsorted List: " + n.toString());
         quickSort(n);
