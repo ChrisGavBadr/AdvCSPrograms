77ae04d3a9fc463fbb20c71420f47a113a9a11b8
Christian Baduria <christian.baduria@gmail.com>
2020-02-07 12:46:09 -0500

    Began working on classwork 8

---
+++ src/classwork/cw8/PostfixEvaluate.java
@@ -0,0 +1,41 @@
+package classwork.cw8;
+
+import java.util.*;
+
+public class PostfixEvaluate {
+
+    public static void main(String[] args) {
+        Queue<String> expressions = new LinkedList<String>(Arrays.asList("1 2 3 * + 4 +", "2 3 1 * + 9 -", "3 4 /"));
+
+        while (!expressions.isEmpty()) {
+            String pfexpr = expressions.remove();
+            System.out.println(pfexpr + " = " + postfixEvaluate(pfexpr));
+        }
+    }
+
+    public static double postfixEvaluate(String expression) {
+        String[] tokens = expression.split(" ");
+        Stack<Double> numbers = new Stack<>();
+
+        for (String token : tokens) {
+            switch (token) {
+                case "*":
+                    numbers.push(numbers.pop() * numbers.pop());
+                    break;
+                case "/":
+                    numbers.push(1 / (double) numbers.pop() * numbers.pop());
+                    break;
+                case "+":
+                    numbers.push(numbers.pop() + numbers.pop());
+                    break;
+                case "-":
+                    numbers.push(-numbers.pop() + numbers.pop());
+                    break;
+                default:
+                    numbers.push(Double.parseDouble(token));
+            }
+        }
+
+        return numbers.pop();
+    }
+}
---
+++ src/classwork/cw8/SplitQueue.java
@@ -0,0 +1,10 @@
+package classwork.cw8;
+
+public class SplitQueue {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static
+}
